a3535cc903ea713df483b8f469467a64fa390d9d
Evan Hensleigh <futuraprime@gmail.com>
2016-05-20 11:39:24 +0100

    test: tests now functioning properly.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import mapValues from 'lodash/mapValues';
  *
  * @param  {object} map - a map of props for a component to locations
  *                        on the store. Can use dot notation.
+ * @param  {function} reduxConnect - should always equal connect. is a
+ *                                   parameter strictly for testing
+ *                                   purposes & should never be set
  *
  * @return {function} - the connection function for this map (to apply
  *                      to the component)
  */
-export default function connectMap(map) {
-  return connect((state, ownProps) => {
+export default function connectMap(map, reduxConnect = connect) {
+  return reduxConnect((state, ownProps) => {
     const connectedProps = mapValues(map, (value, key) => {
       const keys = value.split('.');
       while (key = keys.shift()) { // eslint-disable-line no-cond-assign
--- test/index.js
+++ test/index.js
@@ -1,15 +1,37 @@
 import 'babel-polyfill';
 import connectMap from '../src';
-import { connect } from 'react-redux';
 import chai from 'chai';
 import spies from 'chai-spies';
 chai.use(spies);
 chai.should();
 
+function getSpyCall(spy, i = 0) {
+  return spy.__spy.calls[i]; // eslint-disable-line no-underscore-dangle
+}
+
 describe('connectMap', () => {
+  let connectSpy = null;
+  beforeEach(() => {
+    connectSpy = chai.spy('connect');
+  });
   it('should connect appropriately', () => {
-    const connectSpy = chai.spy(connect);
-    connectMap({ 'foo': 'bar' });
-    connectSpy.should.have.been.called();
+    connectMap({ 'foo': 'bar' }, connectSpy);
+    connectSpy.should.have.been.called(1);
+    const callArgs = getSpyCall(connectSpy);
+    callArgs[0].should.be.a('function');
+  });
+
+  describe('callback checking', () => {
+    it('should provide a working callback', () => {
+      connectMap({ 'foo': 'bar' }, connectSpy);
+      const mapStateToProps = getSpyCall(connectSpy)[0];
+      mapStateToProps({ 'bar': 1 }).should.eql({ 'foo': 1 });
+    });
+
+    it('should be able to fetch nested properties', () => {
+      connectMap({ 'foo': 'bar.baz' }, connectSpy);
+      const mapStateToProps = getSpyCall(connectSpy)[0];
+      mapStateToProps({ 'bar': { 'baz': 10 } }).should.eql({ 'foo': 10 });
+    });
   });
 });
